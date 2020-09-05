6a1a34171720caab51c2e5a2ea851d64e76a20ee
Nikita <nikita@MacBook-Pro-Nikita.local>
2020-09-05 11:19:04 +0300

    add error message about ES supporting

--- src/actions/visitVariableDeclarator.js
+++ src/actions/visitVariableDeclarator.js
@@ -1,6 +1,10 @@
 const index = require("./index");
 
+const availableKinds = ["var"];
+
 module.exports = function (node) {
+  if (!availableKinds.includes(node.kind))
+    throw new Error("Available ECMAscript: ES5");
   const id = index.visitNode(node.id);
   const initValue = index.visitNode(node.init);
   const { globalScope } = global;
--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 function foobar(a) {
-  const b = 32;
-  const c = b - 25;
+  var b = 32;
+  var c = b - 25;
   return c * a;
 }
 
-const res = foobar(4);
+var res = foobar(4);
 
 print(res);
